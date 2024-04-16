ccfd5f56aab674f6f6142770c0400e78d95be203
Данила Тяпкин <dtapkin6@gmail.com>
2024-04-16 12:46:10 +0300

    Ну типа вот так вот да

--- Complex_Num/Program.cs
+++ Complex_Num/Program.cs
@@ -3,11 +3,12 @@
     class Complex
     {
         public double r, i;
+        public string sign;
 
-        public static string Sum(double a1, double b1, double a2, double b2) => Convert.ToString($"Результат сложения: {a1 + a2} {b1 + b2}i");
-        public static string Subtraction(double a1, double b1, double a2, double b2) => Convert.ToString($"Результат вычитания: {a1 - a2} {b1 - b2}i");
-        public static string Multiply(double a1, double b1, double a2, double b2) => Convert.ToString($"Результат умножения: {a1 * a2 - b1 * b2} {b1 * a2 + a1 * b2}i");
-        public static string Division(double a1, double b1, double a2, double b2) => Convert.ToString($"Результат деления: {a1 * a2 + b1 * b2/(a2 * a2 + b2 * b2)} {b1 * a2 - a1 * b2/(a2 * a2 + b2 * b2)}i");
+        public string Sum(double a1, double b1, double a2, double b2) => Convert.ToString($"Результат сложения: {a1 + a2} {b1 + b2}i");
+        public string Subtraction(double a1, double b1, double a2, double b2) => Convert.ToString($"Результат вычитания: {a1 - a2} {b1 - b2}i");
+        public string Multiply(double a1, double b1, double a2, double b2) => Convert.ToString($"Результат умножения: {a1 * a2 - b1 * b2} {b1 * a2 + a1 * b2}i");
+        public string Division(double a1, double b1, double a2, double b2) => Convert.ToString($"Результат деления: {a1 * a2 + b1 * b2/(a2 * a2 + b2 * b2)} {b1 * a2 - a1 * b2/(a2 * a2 + b2 * b2)}i");
     }
     class Program
     {
@@ -16,23 +17,25 @@
             Complex c1 = new();
             Complex c2 = new();
 
-            Console.WriteLine("Введите первое комплексное число в формате {a bi}");
+            Console.WriteLine("Введите первое комплексное число в формате {a + bi}");
             string str = Console.ReadLine();
 
             c1.r = Convert.ToDouble (str.Split(" ")[0]);
-            c1.i = Convert.ToDouble(str.Replace("i", "").Split(" ")[1]);
+            c1.sign = str.Split(" ")[1];
+            c1.i = Convert.ToDouble(c1.sign + Convert.ToDouble(str.Replace("i", "").Split(" ")[2]));
 
             Console.WriteLine("Введите второе комплексное число в формате {a bi}");
             str = Console.ReadLine();
 
             c2.r = Convert.ToDouble(str.Split(" ")[0]);
-            c2.i = Convert.ToDouble(str.Replace("i", "").Split(" ")[1]);
+            c2.sign = str.Split(" ")[1];
+            c2.i = Convert.ToDouble(c2.sign + Convert.ToDouble(str.Replace("i", "").Split(" ")[2]));
 
             Console.WriteLine("Действия с комплексными числами: " +
-                $"\nСложение: {Complex.Sum(c1.r, c1.i, c2.r, c2.i)}\n" +
-                $"\nВычитание: {Complex.Subtraction(c1.r, c1.i, c2.r, c2.i)}" +
-                $"\nУмножение: {Complex.Multiply(c1.r, c1.i, c2.r, c2.i)}" +
-                $"\nДеление: {Complex.Division(c1.r, c1.i, c2.r, c2.i)}");
+                $"\nСложение: {c1.Sum(c1.r, c1.i, c2.r, c2.i)}" +
+                $"\nВычитание: {c1.Subtraction(c1.r, c1.i, c2.r, c2.i)}" +
+                $"\nУмножение: {c1.Multiply(c1.r, c1.i, c2.r, c2.i)}" +
+                $"\nДеление: {c1.Division(c1.r, c1.i, c2.r, c2.i)}");
 
         }
     }
